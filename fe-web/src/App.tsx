54f7f1e1ff412d3fce46b47b50a50d9062f7a648
sehee <didtpgml0627@gmail.com>
2025-10-21 16:25:01 +0900

    feat: 로그인 로직 및 전역 css 파일 수정

--- fe-web/src/App.tsx
+++ fe-web/src/App.tsx
@@ -1,15 +1,44 @@
-import { useLocation, Routes, Route } from "react-router-dom";
-import Join from "./pages/Join";
-
+// src/App.tsx
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useState } from 'react';
+import Join from './pages/Join';
+import Classroom from './pages/Classroom';
+import StudentRoom from './pages/StudentRoom';
 
 export default function App() {
-  const location = useLocation();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
 
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Join />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route 
+        path="/" 
+        element={
+          isLoggedIn ? 
+          <Navigate to="/classroom" replace /> : 
+          <Join onLoginSuccess={handleLogin} />
+        } 
+      />
+      <Route 
+        path="/classroom" 
+        element={
+          isLoggedIn ? 
+          <Classroom /> : 
+          <Navigate to="/" replace />
+        } 
+      />
+      <Route 
+        path="/studentroom/:id" 
+        element={
+          isLoggedIn ? 
+          <StudentRoom /> : 
+          <Navigate to="/" replace />
+        } 
+      />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
