7750d70b5b9138a5cc95c69e6db45c7de658ad3b
양진서 <jinseo_yang@naver.com>
2025-11-03 12:59:23 +0900

    chore: App.tsx 원상복구

--- fe_app/App.tsx
+++ fe_app/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import { Platform } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import MMKVTestScreen from './src/screens/MMKVTestScreen';
+import React from "react";
+import { Platform } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import AppNavigator from "./src/navigation/AppNavigator";
+import { navigationRef } from "./src/navigation/RootNavigation";
+import GlobalVoiceTriggers from "./src/components/GlobalVoiceTriggers";
+import { TriggerProvider } from "./src/triggers/TriggerContext";
 
 export default function App() {
   return (
     <>
-      <MMKVTestScreen />
-      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+      <TriggerProvider>
+        <AppNavigator />
+        <GlobalVoiceTriggers
+          onVoiceCommand={() => {
+            navigationRef.current?.navigate("Question" as never);
+          }}
+        />
+        <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
+      </TriggerProvider>
     </>
   );
 }
